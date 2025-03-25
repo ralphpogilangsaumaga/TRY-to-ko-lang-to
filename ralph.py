ccd920da4bc2249081512e3b7f609944adae39a7
ralphpogilangsaumaga <ralphsanluis8@gmail.com>
2025-03-24 23:54:45 -0400

    - uncomment ng potangina

---
+++ ralph.py
@@ -0,0 +1,30 @@
+#string
+variable = "This is a string"
+print(variable)
+
+print('This is string 2')
+
+#integer
+number = 3
+print(number)
+
+number_1 = 56
+number_2 = 123.3434
+# print(number_1 + number_2)
+# print(number_1 - number_2)
+# print(number_1 // number_2)
+# print(number_1 * number_2)
+
+
+#boolean
+number_1 = True
+if number_1 == number_2:
+    print(number_1, "is less than" ,number_2)
+else:
+    print("This is less than idiot")
+
+
+#string concatonation
+a = "Madijios"
+b = "Oging"
+print(a,"buto",b)
